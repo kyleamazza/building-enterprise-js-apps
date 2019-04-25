4b0d4b92611fda04d8620543e923057d3cdfee31
kyleamazza <kyleamazza@gmail.com>
2019-04-24 21:43:08 -0700

    Handle create user calls with empty payload
    - Added steps file for empty payload functionality.
    - Refactored steps file to use context provided by Cucumber (`this`
    functionality)
    - Added superagent and @babel/node.
    - Added @babel/register for running tests with on-the-fly transpilation
    - Added .env and .env.example.
    - Added dotenv-cli to run scripts with env variables

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,26 +1,53 @@
 import { When, Then } from 'cucumber';
+import assert from 'assert';
+import superagent from 'superagent';
 
-When('the client creates a POST request to /users', function(cb) {
-  cb(null, 'pending');
+// NOTENOTENOTENOETNOTE: If you don't need to run the `callback` function, do NOT include it in the function signature!!!!!!!!!!!!!!!!
+
+// These steps build up the action, which is sent in the final `When` function
+When('the client creates a POST request to /users', function () {
+  const { SERVER_PROTOCOL, SERVER_HOSTNAME, SERVER_PORT } = process.env;
+  const url = `${SERVER_PROTOCOL}://${SERVER_HOSTNAME}:${SERVER_PORT}/users`;
+  this.request = superagent('POST', url);
 });
 
-When('attaches a generic empty payload', function(cb) {
-  cb(null, 'pending');
+When('attaches a generic empty payload', function () {
+  // By default superagent attaches an empty payload if none is specified.
+  return undefined;
 });
 
-When('sends the request', function(cb) {
-  cb(null, 'pending');
+When('sends the request', function (cb) {
+  this.request
+    .then((response) => {
+      this.response = response.res;
+      cb();
+    })
+    .catch((err) => {
+      this.response = err.response;
+      cb();
+    });
 });
 
-Then('our API should respond with a 400 HTTP status code', function(cb) {
-  cb(null, 'pending');
+Then('our API should respond with a 400 HTTP status code', function () {
+  assert.equal(this.response.statusCode, 400);
 });
 
-Then('the payload of the response should be a JSON object', function(cb) {
-  cb(null, 'pending');
+Then('the payload of the response should be a JSON object', function () {
+  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+
+  // We want the Content-Type to be declared as application/json
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of Content-Type: application/json');
+  }
+
+  try {
+    this.responsePayload = JSON.parse(this.response.text);
+  } catch (err) {
+    throw new Error('Response is not a valid JSON object');
+  }
 });
 
-Then('contains a message property which says "Payload should not be empty"', function(cb) {
-  cb(null, 'pending');
+Then('contains a message property which says "Payload should not be empty"', function () {
+  assert.equal(this.responsePayload.message, 'Payload should not be empty');
 });
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import http from 'http';
 
 function requestHandler(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.method === 'POST' && req.url === '/users') {
+    res.writeHead(400, {
+      'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify({'message': 'Payload should not be empty'}));
+    return;
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
   res.end('Hello world');
 }
 
 const server = http.createServer(requestHandler);
 
-server.listen(3000, () => {
-  console.log('Server listening on 3000...');
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server listening on port ${process.env.SERVER_PORT}`);
 });
