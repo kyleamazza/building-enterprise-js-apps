ef5b9ff1dafe72f145f85c3d84c8e024c2750a9e
kyleamazza <kyleamazza@gmail.com>
2019-05-10 16:13:43 -0700

    Complete unit tests for generateValidationErrorMessage

--- src/validators/errors/messages/index.unit.test.js
+++ src/validators/errors/messages/index.unit.test.js
@@ -17,4 +17,49 @@ describe('generateValidationErrorMessage', function () {
 
     expect(expected).to.be.equal(actual);
   });
+
+  it('should return the correct string when `error.keyword` is "type"', function () {
+    const errors = [{
+      keyword: 'type',
+      dataPath: '.test.path',
+      params: {
+        type: 'string'
+      }
+    }];
+
+    const actual = generateValidationErrorMessage(errors);
+    const expected = "The '.test.path' field must be of type 'string'";
+
+    expect(expected).to.be.equal(actual);
+  });
+
+  it('should return the correct string when `error.keyword` is "format"', function () {
+    const errors = [{
+      keyword: 'format',
+      dataPath: '.test.path',
+      params: {
+        format: 'email'
+      }
+    }];
+
+    const actual = generateValidationErrorMessage(errors);
+    const expected = "The '.test.path' field must be a valid email";
+
+    expect(expected).to.be.equal(actual);
+  });
+
+  it('should return the correct string when `error.keyword` is "additionalProperties"', function () {
+    const errors = [{
+      keyword: 'additionalProperties',
+      dataPath: '.test.path',
+      params: {
+        additionalProperty: 'email'
+      }
+    }];
+
+    const actual = generateValidationErrorMessage(errors);
+    const expected = "The '.test.path' object does not support the field 'email'";
+
+    expect(expected).to.be.equal(actual);
+  });
 });
